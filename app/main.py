4cb5e7bb1e2331b5ee887535aeb042cc8be04898
Dhanush Kumar <dhanushdarapureddy9358@gmail.com>
2026-01-30 13:03:27 +0530

    update index.html

--- app/main.py
+++ app/main.py
@@ -1,20 +1,18 @@
 from fastapi import FastAPI
-from fastapi.responses import StreamingResponse, HTMLResponse
+from fastapi.responses import StreamingResponse, HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from app.schemas import PurchaseOrderRequest
 from app.services.pdf_service import generate_po_pdf
 
 app = FastAPI(title="Purchase Order PDF Generator")
 
+# Serve static assets (logo, signature)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
 def home():
-    return """
-    <h2>Purchase Order PDF API</h2>
-    <p>POST /api/generate-po</p>
-    """
+    return FileResponse("frontend/index.html")
 
 
 @app.post("/api/generate-po")
